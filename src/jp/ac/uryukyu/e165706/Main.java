62d95645725e0873459d1c9dac4784f396891342
Yamato Goto <e165706@ie.u-ryukyu.ac.jp>
2017-02-14 10:42:00 +0900

    2017 2/14(Tue) report5_reup step1 finish

---
+++ src/jp/ac/uryukyu/e165706/Main.java
@@ -0,0 +1,9 @@
+package jp.ac.uryukyu.e165706;
+
+
+public class Main {
+    public static  void main(String args[]) {
+        String str = null;
+        System.out.println(str.length());
+    }
+}
