13f8811e592faa8461b30914e93117ce3164689b
Yamato Goto <e165706@ie.u-ryukyu.ac.jp>
2017-02-17 12:16:59 +0900

    2017 2/17(Fri)11:58 report5_reup step3 finish

--- src/jp/ac/uryukyu/e165706/Main.java
+++ src/jp/ac/uryukyu/e165706/Main.java
@@ -2,8 +2,10 @@ package jp.ac.uryukyu.e165706;
 
 public class Main {
     public static  void main(String args[]) {
-        String str = null;
-        try {System.out.println(str.length());}
+        String str = "3．14";
+        try {System.out.println(str.length());
+          double value = Double.parseDouble(str);
+        }
         catch (Exception e){
             //System.err.print("Err is " + e);
             e.printStackTrace();
