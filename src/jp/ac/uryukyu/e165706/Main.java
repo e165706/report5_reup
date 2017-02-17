8e44494de4ec13f8fc049f924db004122e67e987
Yamato Goto <e165706@ie.u-ryukyu.ac.jp>
2017-02-17 11:58:55 +0900

    2017 2/17(Fri)11:58 report5_reup step2 finish

--- src/jp/ac/uryukyu/e165706/Main.java
+++ src/jp/ac/uryukyu/e165706/Main.java
@@ -3,6 +3,10 @@ package jp.ac.uryukyu.e165706;
 public class Main {
     public static  void main(String args[]) {
         String str = null;
-        System.out.println(str.length());
+        try {System.out.println(str.length());}
+        catch (Exception e){
+            //System.err.print("Err is " + e);
+            e.printStackTrace();
+        }
     }
 }
